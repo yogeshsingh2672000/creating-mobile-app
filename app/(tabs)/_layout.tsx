0fa16e0f175c60a8548547203a4bb10ba2c1f0b3
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2025-05-11 17:33:38 +0530

    :tada: first initial commit in working state

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,23 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { images } from "@/constants/icons";
+import { Tabs } from "expo-router";
+import React from "react";
+import { Image, StyleSheet } from "react-native";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const _layout = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+    <Tabs>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Dashboard",
+          tabBarIcon: () => <Image className="size-6" source={images.camera} />,
+          //   headerShown: false,
         }}
       />
     </Tabs>
   );
-}
+};
+
+export default _layout;
+
+const styles = StyleSheet.create({});
---
+++ constants/icons.ts
@@ -0,0 +1,6 @@
+import camera from "@/assets/icons/camera.png";
+import history from "@/assets/icons/history.png";
+import home from "@/assets/icons/home.png";
+import profile from "@/assets/icons/profile.png";
+
+export const images = { history, home, profile, camera };
